b667df1f373e6dbc5059f5d8f36685e59d3b4cee
bletchking87 <rapple05@gmail.com>
2026-05-08 12:22:34 +0100

    Improved time formatting

--- app.py
+++ app.py
@@ -224,9 +224,9 @@ if st.session_state.orig is not None:
         
         col1, col2 = st.columns(2)
         with col1:
-            st.metric(label="Fast Route", value=f"{len_fast/1000:.1f} km", delta=(fast_time), delta_color="inverse")
+            st.metric(label="Fast Route", value=f"{len_fast/1000:.1f} km", delta=format_time(int(fast_time)), delta_color="inverse")
         with col2:
-            st.metric(label=f"({k_label} Route)", value=f"{len_quiet/1000:.1f} km", delta=quiet_time)
+            st.metric(label=f"({k_label} Route)", value=f"{len_quiet/1000:.1f} km", delta=format_time(int(quiet_time)))
         
         
     
